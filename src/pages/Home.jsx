1bed9ecb4d03c30a50d18d5859895213db12eaae
oladokunlt <lukmanoladokun86@gmail.com>
2023-10-21 09:20:07 +0100

    rounting to single project done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { ProductContext } from "../contexts/ProductContext";
+import Product from "../components/Product";
 
 const Home = () => {
   const { products } = useContext(ProductContext);
@@ -17,13 +18,9 @@ const Home = () => {
     <div className="">
       <section className="py-16">
         <div className=" container mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
             {filteredProducts.map((product) => {
-              return (
-                <div key={product.id} className=" h-[300px] bg-pink-200">
-                  {product.title}
-                </div>
-              );
+              return <Product product={product} key={product.id} />;
             })}
           </div>
         </div>
