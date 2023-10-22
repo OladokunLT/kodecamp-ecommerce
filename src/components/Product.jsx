e7388cb1002df08b1f9cd9aa92e0cdb10f871c62
oladokunlt <lukmanoladokun86@gmail.com>
2023-10-22 18:44:15 +0100

    ch: created context for sidebar

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,7 +7,7 @@ const Product = ({ product }) => {
   const { id, image, category, title, price } = product;
   return (
     <div>
-      <div className="border border-[#e4e4e4] h-[300px] mb-4 relateve overflow-hidden group transition ">
+      <div className="border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition ">
         <div className="w-full h-full flex justify-center items-center">
           {/* image */}
           <div className="w-[200px] mx-auto flex justify-center items-center ">
@@ -19,7 +19,7 @@ const Product = ({ product }) => {
           </div>
         </div>
         {/* buttons */}
-        <div className="absolut top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
+        <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
           <button>
             <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
               <BsPlus className="text-3xl" />
